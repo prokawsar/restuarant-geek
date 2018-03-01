d7441b6b78214b66284954e8cbee7a22aeca9f3b
Kawsar Ahmed <mkawsar10@gmail.com>
2018-03-01 14:30:05 +0600

    Waiter view worked, Previous commit mistake

--- resources/views/waiter/home.blade.php
+++ resources/views/waiter/home.blade.php
@@ -3,38 +3,83 @@
 @extends('layouts.waiter')
 
 @section('content')
-<div class="container">
-    
-    <div class="row">
-        <div class="col-md-12">
-                    
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                @if (session('danger'))
+                    <div class="alert alert-danger">
+                        {{ session('danger') }}
+                    </div>
+                @endif
+
+                @php $categories = App\Category::where('rest_id', Auth::guard('waiter')->user()->rest_id)->get(); @endphp
+                @php $name = App\User::select('rest_name')->where('id', Auth::guard('waiter')->user()->rest_id)->get(); @endphp
+
+                <div class="row">
+                    <h3 class="text-center"> You are on <span class="text-success">{{ $name[0]->rest_name }} </span>
+                        Restaurant </h3>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <button class="btn btn-default pull-left" id="collapse-all">Expand All</button>
+                    </div>
+                    <br>
+                    @foreach ($categories as $category)
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="panel panel-info">
+                                    <div class="panel-heading text-bold text-center" data-toggle="collapse"
+                                         data-target="#demo{{ $category->id }}">{{ $category->cat_name }} </div>
+
+                                    <div class="panel-body collapse coll-all" id="demo{{ $category->id }}">
+
+
+                                        @php $items = App\Item::where('cat_id', $category->id)->get(); @endphp
+
+                                        <table class="table table-hover">
+                                            <thead>
+                                            <tr>
+
+                                                <th scope="col">Item Name</th>
+                                                <th scope="col">Price</th>
+                                                <th scope="col">Action</th>
+
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            @foreach ($items as $item)
+                                                <tr>
+                                                    <th scope="row">{{ $item->item_name}}</th>
+                                                    <td>{{ $item->price }}</td>
+                                                    <td><a href="#" class="btn btn-info">Place Order</a></td>
+
+                                                </tr>
+                                            @endforeach
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
+                    @endforeach
+                </div>
 
-                    @if (session('danger'))
-                        <div class="alert alert-danger">
-                            {{ session('danger') }}
+                <div class="col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-heading text-bold text-center">
+                            Current Order Items
                         </div>
-                    @endif
-            
-            @php $categories = App\Category::where('rest_id', Auth::guard('waiter')->user()->rest_id)->get(); @endphp
-            @php $name = App\User::select('rest_name')->where('id', Auth::guard('waiter')->user()->rest_id)->get(); @endphp
-           
-           <div class="row">
-               <h3 class="text-center"> You are on <span class="text-success">{{ $name[0]->rest_name }} </span> Restaurant </h3>
-            </div>
-            @foreach ($categories as $category)
-            <div class="col-md-6">
-                <div class="panel panel-info">
-                    <div class="panel-heading text-bold text-center">{{ $category->cat_name }} </div>
 
-                <div class="panel-body">
-                    
-
-                        @php $items = App\Item::where('cat_id', $category->id)->get(); @endphp
+                        <div class="panel-body">
 
                             <table class="table table-hover">
                                 <thead>
@@ -42,27 +87,81 @@
 
                                     <th scope="col">Item Name</th>
                                     <th scope="col">Price</th>
-                                    <th scope="col">Action</th>
-                                   
+
                                 </tr>
                                 </thead>
                                 <tbody>
                                 @foreach ($items as $item)
-                                <tr>
-                                    <th scope="row">{{ $item->item_name}}</th>
-                                    <td>{{ $item->price }}</td>
-                                    <td><a href="#" class="btn btn-info" >Place Order</a></td>
-                                    
-                                </tr>
+                                    <tr>
+                                        <th scope="row">{{ $item->item_name}}</th>
+                                        <td>{{ $item->price }}</td>
+                                    </tr>
                                 @endforeach
-
+                                <tr>
+                                    <th scope="row">Total</th>
+                                    <td>500</td>
+                                </tr>
                                 </tbody>
                             </table>
+                            <div class="form-group">
+
+                                <label class="control-label" for="title">Select Table:</label>
+                                @php $table = App\Table::where('rest_id', Auth::guard('waiter')->user()->rest_id)->get(); @endphp
+
+                                <select class="form-control" name="cat_id" required>
+                                    <option>Select Table</option>
+
+                                    @foreach ($table as $name)
+                                        <option value="{{ $name->id}}">{{ $name->name_or_no}}</option>
+                                    @endforeach
+
+                                </select>
+                                <div class="help-block with-errors"></div>
+
+                            </div>
+
+                            <div class="form-group">
+
+                                <label class="control-label" for="title">Customer Name:</label>
+
+                                <input type="text" id="title" name="title" class="form-control"
+                                       data-error="Please enter name." required/>
+
+                                <div class="help-block with-errors"></div>
+
+                            </div>
+                            <div class="form-group">
+
+                                <label class="control-label" for="title">Customer Phone:</label>
+
+                                <input type="number" id="title" name="title" class="form-control"
+                                       data-error="Please enter phone." required/>
+
+                                <div class="help-block with-errors"></div>
+
+                            </div>
+
+                            <button class="btn btn-success pull-right">Send to Kitchen</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            @endforeach
         </div>
     </div>
-</div>
 @endsection
+<script src="{{asset('https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js')}}"></script>
+<script>
+    $( document ).ready(function() {
+        $('#collapse-all').on('click', function () {
+            $('.coll-all').collapse('toggle');
+            if($("#collapse-all").text() == "Expand All") {
+                $("#collapse-all").text('Collapse All');
+            }else {
+                $("#collapse-all").text('Expand All');
+            }
+
+
+        });
+    });
+
+</script>
